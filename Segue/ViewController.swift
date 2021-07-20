e077e7cdf0416b573b4fa6bad08bfad1ee7ee682
Emmett <ekennedy@cps.edu>
2021-07-20 10:41:10 -0500

    Initial Commit

---
+++ Segue/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Segue
+//
+//  Created by Emmett Kennedy on 7/20/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
