3aed68a21d4b1a002e35f879ee208a704b137845
Emmett <ekennedy@cps.edu>
2021-07-20 19:23:21 -0500

    Segue

---
+++ Segue/HomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  HomeViewController.swift
+//  Segue
+//
+//  Created by Emmett Kennedy on 7/20/21.
+//
+
+import UIKit
+
+class HomeViewController: ViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
